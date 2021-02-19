8e85f7bab2dd6c313072f2b4e721c828c35be672
TKDKid1000 <69736165+TKDKid1000@users.noreply.github.com>
2021-02-19 11:04:50 -0800

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,22 +1,24 @@
 var table = document.getElementById("repos");
 let xhr = new XMLHttpRequest();
-            xhr.open('GET', "data/repositories.json", true);
+            xhr.open('GET', "https://raw.githubusercontent.com/TKDKid1000/tkdkid1000.github.io/master/data/repositories.json", true);
             xhr.send();
 
             xhr.onreadystatechange = processRequest;
             function processRequest(e) {
                 if (xhr.readyState == 4 && xhr.status == 200) {
                     let response = JSON.parse(xhr.responseText);
+                    console.log(response);
                     response.repo.forEach((repo) => {
                         var row = table.insertRow(-1);
                         var name = row.insertCell(-1);
-                        name.innerHTML = response.name;
+                        name.innerHTML = repo.name;
+                        alert(response);
                         var url = row.insertCell(-1);
-                        url.innerHTML = response.url;
+                        url.innerHTML = repo.url;
                         var description = row.insertCell(-1);
-                        description.innerHTML = response.description;
+                        description.innerHTML = repo.description;
                         var language = row.insertCell(-1);
-                        language.innerHTML = response.language;
+                        language.innerHTML = repo.language;
                     });
                 }
             }
