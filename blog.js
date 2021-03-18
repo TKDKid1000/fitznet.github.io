cba15724fea7a8acb54a72448a9e58ec82b4c2b8
TKDKid1000 <rhoneeiler@gmail.com>
2021-03-18 10:16:10 -0700

    added full blog

--- blog.js
+++ blog.js
@@ -1,36 +1,27 @@
-let table = document.getElementById("repos");
-let xhr = new XMLHttpRequest();
-            xhr.open('GET', "https://api.github.com/users/TKDKid1000/repos", true);
-            xhr.send();
-
-            xhr.onreadystatechange = processRequest;
-            function processRequest(e) {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    let response = JSON.parse(xhr.responseText);
-                    response.forEach((repo) => {
-                        let row = table.insertRow(1);
-                        let name = row.insertCell(0);
-                        name.innerHTML = "<p>"+repo.full_name+"</p>";
-                        let url = row.insertCell(-1);
-                        url.innerHTML = "<a href=\""+repo.html_url+"\">"+repo.html_url+"</a>";
-                        let description = row.insertCell(-1);
-                        if (repo.description !== null) {
-                            description.innerHTML = "<p>"+repo.description+"</p>";
-                        } else {
-                            description.innerHTML = "<p>The repository has no description.</p>";
-                        }
-                        let language = row.insertCell(-1);
-                        if (language !== null) {
-                            language.innerHTML = "<p>"+repo.language+"</p>";
-                        } else {
-                            language.innerHTML = "<p>This repository doesn't have a primary language.</p>";
-                        }
-                        let license = row.insertCell(-1);
-                        if (repo.license !== null) {
-                            license.innerHTML = "<p>"+repo.license.name+"</p>"
-                        } else {
-                            license.innerHTML = "<p>This repository has no license.</p>";
-                        }
-                    });
+function loadblog(divid, number) {
+    const div = document.getElementById(divid);
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', "/data/blog.json", true);
+    xhr.send()
+    xhr.onreadystatechange = processRequest;
+    function processRequest(e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            let response = JSON.parse(xhr.responseText);
+            if (response.length-1 < number) {
+                div.querySelector("#posttitle").innerHTML = "404 Post not Found";
+                var content = ["No post here!", "Please hold on!"];
+                div.querySelector("#datetime").innerHTML = "No date or time here!";
+                div.querySelector("#permalink").innerHTML = "Blog Home";
+            } else {
+                div.querySelector("#posttitle").innerHTML = response[number]["title"];
+                for (var x=0; x<response[number]["content"].length; x++) {
+                    var element = document.createElement("p");
+                    element.appendChild(document.createTextNode(response[number]["content"][x]));
+                    div.querySelector("#postcontent").appendChild(element);
                 }
+                div.querySelector("#datetime").innerHTML = `<i class="fas fa-book"></i> ${response[number]["readtime"]} minutes | <i class="fas fa-calendar-alt"></i> ${response[number]["date"]}`;
+                div.querySelector("#permalink").href = `/post.html?post=${number}`;
             }
+        }
+    }
+}
